5401fd45b51cf42ba7d1364ad8a38167eb2eb8db
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-08-27 21:31:18 +0000

    Add frontend url

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ const app = express();
 const ALLOWED_ORIGINS = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'https://degree-audit-frontend.vercel.app/'
 ];
 
 app.use((req, res, next) => {
